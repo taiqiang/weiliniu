e07316e8a297039ec98116edcb83d408dcfc65c3
台强强 <taiqiangqiang@autohome.com.cn>
2016-06-15 16:56:40 +0800

    提交数据

--- shop/shop_tbschedule/src/test/java/com/taobao/pamirs/schedule/test/ZookeeperTest.java
+++ shop/shop_tbschedule/src/test/java/com/taobao/pamirs/schedule/test/ZookeeperTest.java
@@ -11,8 +11,6 @@ import org.apache.zookeeper.ZooKeeper;
 import org.apache.zookeeper.data.ACL;
 import org.apache.zookeeper.data.Id;
 import org.apache.zookeeper.server.auth.DigestAuthenticationProvider;
-import org.junit.Test;
-
 import com.taobao.pamirs.schedule.zk.ScheduleWatcher;
 import com.taobao.pamirs.schedule.zk.ZKTools;
 
