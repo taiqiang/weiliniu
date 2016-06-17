ac36d788232929c00dfb3e5a28b6695b89c679d6
台强强 <taiqiangqiang@autohome.com.cn>
2016-06-17 13:27:46 +0800

    提交代码

--- shop/shop_tbschedule/src/main/java/com/taobao/pamirs/businessdemo/controller/ProtocolMailTask.java
+++ shop/shop_tbschedule/src/main/java/com/taobao/pamirs/businessdemo/controller/ProtocolMailTask.java
@@ -3,33 +3,35 @@ package com.taobao.pamirs.businessdemo.controller;
 import java.util.ArrayList;
 import java.util.Comparator;
 import java.util.List;
+
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
-import com.taobao.pamirs.businessdemo.model.scheduleTest;
+
+import com.taobao.pamirs.businessdemo.model.ScheduleTest;
 import com.taobao.pamirs.schedule.IScheduleTaskDealSingle;
 import com.taobao.pamirs.schedule.TaskItemDefine;
 
-public class ProtocolMailTask implements IScheduleTaskDealSingle<scheduleTest> {
+public class ProtocolMailTask implements IScheduleTaskDealSingle<ScheduleTest> {
 
 	private final Logger logger = LoggerFactory.getLogger(getClass());
 	
 	
 	@Override
-	public Comparator<scheduleTest> getComparator() {
+	public Comparator<ScheduleTest> getComparator() {
 		return null;
 	}
 
 	@Override
-	public List<scheduleTest> selectTasks(String taskParameter, String ownSign,
+	public List<ScheduleTest> selectTasks(String taskParameter, String ownSign,
 			int taskItemNum, List<TaskItemDefine> taskItemList,
 			int eachFetchDataNum)
 			throws Exception {
-		List<scheduleTest> scheduleTestList = new ArrayList<scheduleTest>();
+		List<ScheduleTest> scheduleTestList = new ArrayList<ScheduleTest>();
 		return scheduleTestList;
 	}
 
 	@Override
-	public boolean execute(scheduleTest scheduleTest, String ownSign) throws Exception {
+	public boolean execute(ScheduleTest scheduleTest, String ownSign) throws Exception {
 		try{
 			return true;
 		}catch(Exception e){
